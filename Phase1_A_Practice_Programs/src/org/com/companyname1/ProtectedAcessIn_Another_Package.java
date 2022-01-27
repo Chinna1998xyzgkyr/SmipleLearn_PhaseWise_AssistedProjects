97b6cda12b9a971ead841e27f0302876eec2106c
Chinna1998xyzgkyr <97274492+Chinna1998xyzgkyr@users.noreply.github.com>
2022-01-27 19:33:49 +0530

    Add files via upload
    first commit

---
+++ Phase1_A_Practice_Programs/src/org/com/companyname1/ProtectedAcessIn_Another_Package.java
@@ -0,0 +1,14 @@
+package org.com.companyname1;
+import org.com.companyname.*;
+public class ProtectedAcessIn_Another_Package extends ProtectedAcessModifier{
+
+
+		public static void main(String[] args) {
+			ProtectedAcessIn_Another_Package obj = new ProtectedAcessIn_Another_Package ();   
+		       obj.display();  
+		}
+
+	}
+
+
+
---
+++ Phase1_A_Practice_Programs/src/org/com/companyname1/PublicAcessModifier_In_Anthor_Class.java
@@ -0,0 +1,13 @@
+package org.com.companyname1;
+import org.com.companyname.*;
+
+public class PublicAcessModifier_In_Anthor_Class {
+
+		public static void main(String[] args) {
+			
+			PublicAcessModifier obj = new PublicAcessModifier(); 
+	        obj.display();  
+			
+		}
+
+}
